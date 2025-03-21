a15cbb93200ebf1a0059d77a15b754478dfe7c1d
takehito <takehito.koshimizu@gmail.com>
2025-03-21 16:23:45 +0900

    Add an empty project

---
+++ MyToybox/MyToyboxTests/MyToyboxTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyToyboxTests.swift
+//  MyToyboxTests
+//
+//  Created by Takehito Koshimizu on 2025/03/21.
+//
+
+import Testing
+
+struct MyToyboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
